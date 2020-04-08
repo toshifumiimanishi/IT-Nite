fbb05813579b393458f360f4e8456df7d0f16f10
Imanishi Toshifumi <13635766+toshifumiimanishi@users.noreply.github.com>
2020-04-08 22:57:37 +0900

    記事詳細ページのデザイン調整

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,21 +1,80 @@
 import React from "react";
 import { graphql } from "gatsby"
+import styled from "styled-components"
 
-export default (props) => {
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+export default ({ data }) => {
   const {
     title,
-    body
-  } = props.data.microcmsPosts
+    body,
+    createdAt,
+    _embedded
+  } = data.microcmsPosts
+
+  const Title = styled.h1`
+    font-size: 36px;
+    text-align: center;
+  `
+
+  const Article = styled.article`
+    padding: 20px 0;
+
+    a {
+      color: var(--link-color);
+
+      &[target="_blank"] {
+        &::after {
+          content: "\\e900";
+          margin-left: 5px;
+          font-family: 'icomoon' !important;
+          font-size: 0.8em;
+          font-style: normal;
+          font-weight: normal;
+          font-variant: normal;
+          text-transform: none;
+          line-height: 1;
+        }
+      }
+
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  `
+
+  const Time = styled.div`
+    margin: 12px 0;
+    font-size: 12px;
+    text-align: center;
+  `
+
+  const toLocaleDate = (date) => {
+    return new Date(date).toLocaleDateString()
+  }
 
   return (
-    <>
-      <h1>{ title }</h1>
-      <div
-        dangerouslySetInnerHTML={{
-          __html: `${body}`,
-        }}
-      />
-    </>
+    <Layout>
+      <SEO title={ title } />
+      <div>
+        <img
+          src={ _embedded.url }
+          alt=""
+        />
+      </div>
+      <Article>
+        <Title>{ title }</Title>
+        <Time>
+          <time dateTime={ createdAt }>{ toLocaleDate(createdAt) } 公開</time>
+        </Time>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: `${body}`,
+          }}
+        />
+      </Article>
+    </Layout>
   )
 }
 
@@ -24,6 +83,10 @@ export const pageQuery = graphql`
     microcmsPosts(id: { eq: $slug }) {
       title
       body
+      createdAt
+      _embedded {
+        url
+      }
     }
   }
 `
