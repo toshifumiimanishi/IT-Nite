9f5cf10584317fdbcf3532dcb1a9d9172040e14d
Imanishi Toshifumi <13635766+toshifumiimanishi@users.noreply.github.com>
2020-04-16 16:18:31 +0900

    gatsby-plugin-google-gtag プラグインの指定

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,6 +47,14 @@ module.exports = {
         userName: `toshifumiimanishi`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          `${process.env.GA_TRACKING_ID}`
+        ]
+      }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
