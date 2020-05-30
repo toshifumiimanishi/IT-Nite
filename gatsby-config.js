67322d92e2c53b678c929f0849fcc5856457d064
toshifumiimanishi <13635766+toshifumiimanishi@users.noreply.github.com>
2020-05-30 11:59:27 +0900

    gatsby-source-graphql プラグインの読み込み

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,6 +55,17 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-source-graphql`,
+      options: {
+        typeName: "GitHub",
+        fieldName: "github",
+        url: "https://api.github.com/graphql",
+        headers: {
+          Authorization: `Bearer ${process.env.GITHUB_ACCESS_TOKEN}`,
+        },
+      }
+    },
+    {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         trackingIds: [`${process.env.GA_TRACKING_ID}`],
