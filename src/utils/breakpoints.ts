86d68366398459df71bef44526414a3579dc8ef2
toshifumiimanishi <13635766+toshifumiimanishi@users.noreply.github.com>
2020-06-02 17:42:01 +0900

    メディアクエリの便利関数の実装

--- src/utils/breakpoints.ts
+++ src/utils/breakpoints.ts
@@ -1,16 +1,28 @@
 import { breakpoints } from '../constants/breakpoints'
 
-const breakpointMin = (name: keyof typeof breakpoints) => {
+type Breakpoints = keyof typeof breakpoints
+
+const breakpointNext = (name: Breakpoints) => {
+  const breakpointNames  = Object.keys(breakpoints) as Breakpoints[]
+  const nextIndex = breakpointNames.indexOf(name) + 1
+  if (nextIndex < breakpointNames.length) {
+    return breakpointNames[nextIndex]
+  } else {
+    return null
+  }
+}
+
+const breakpointMin = (name: Breakpoints) => {
   const min = breakpoints[name]
   return min !== 0 ? min : null
 }
 
-const breakpointMax = (name: keyof typeof breakpoints) => {
+const breakpointMax = (name: Breakpoints) => {
   const max = breakpoints[name]
   return max && max > 0 ? max - 0.02 : null
 }
 
-export const breakpointUp = (name: keyof typeof breakpoints) => {
+export const breakpointUp = (name: Breakpoints) => {
   const min = breakpointMin(name)
   if (min) {
     return `@media (min-width: ${min}px)`
@@ -19,11 +31,41 @@ export const breakpointUp = (name: keyof typeof breakpoints) => {
   }
 }
 
-export const breakpointDown = (name: keyof typeof breakpoints) => {
+export const breakpointDown = (name: Breakpoints) => {
   const max = breakpointMax(name)
   if (max) {
     return `@media (max-width: ${max}px)`
   } else {
     return '&'
+  }
+}
+
+export const breakpointBetween = (lower: Breakpoints, upper: Breakpoints) => {
+  const min = breakpointMin(lower)
+  const max = breakpointMax(upper)
+
+  if (min !== null && max !== null) {
+    return `@media (min-width: ${min}px) and (max-width: ${max}px)`
+  } else if (max === null) {
+    return breakpointUp(lower)
+  } else if (min === null) {
+    return breakpointDown(upper)
+  }
+}
+
+export const breakpointOnly = (name: Breakpoints) => {
+  const next = breakpointNext(name)
+
+  if (next === null) return breakpointUp(name)
+
+  const min = breakpointMin(name)
+  const max = breakpointMax(next)
+
+  if (min !== null && max !== null) {
+    return `@media (min-width: ${min}px) and (max-width: ${max}px)`
+  } else if (max == null) {
+    return breakpointUp(name)
+  } else if (min == null) {
+    return breakpointDown(next)
   }
 }
