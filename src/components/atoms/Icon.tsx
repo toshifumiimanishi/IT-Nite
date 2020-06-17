55075d95e317a80873f137ed13a22c2936cbea93
Imanishi Toshifumi <13635766+toshifumiimanishi@users.noreply.github.com>
2020-06-18 08:33:33 +0900

    アイコンコンポーネントの作成

---
+++ src/components/atoms/Icon.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import styled from 'styled-components'
+import { faMoon } from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon, FontAwesomeIconProps } from '@fortawesome/react-fontawesome'
+
+type DOMProps = FontAwesomeIconProps
+
+type PresenterProps = {
+  icon: FontAwesomeIconProps['icon']
+}
+
+type ContainerProps = {
+  presenter: React.FC<PresenterProps>
+  icon: string
+}
+
+type Props = {
+  icon: string
+  className?: string
+  title?: string
+}
+
+const IconDOM: React.FC<DOMProps> = ({
+  icon,
+  ...props
+}) => (
+  <FontAwesomeIcon icon={icon} {...props} />
+)
+
+const PresentationalIcon = styled(IconDOM)``
+
+const ContainerIcon: React.FC<ContainerProps> = ({
+  presenter,
+  icon: name,
+  ...props
+}) => {
+  const iconMap = {
+    faMoon,
+    faGithub,
+  }
+  const icon = iconMap[name as keyof typeof iconMap]
+  return presenter({ icon, ...props})
+}
+
+const Icon: React.FC<Props> = ({
+  icon,
+  ...props
+}) => (
+  <ContainerIcon
+    presenter={( presenterProps) => (
+      <PresentationalIcon
+        {...presenterProps}
+      />
+    )}
+    icon={icon}
+    {...props}
+  />
+)
+
+export default Icon
