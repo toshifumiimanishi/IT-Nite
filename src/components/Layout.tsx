9534e1f806a00acf7e70b831f6194cdf93d73852
Toshifumi Imanishi <13635766+toshifumiimanishi@users.noreply.github.com>
2020-06-26 13:05:26 +0900

    [WIP] box-shadow の CSS 変数の変更

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,6 +4,7 @@ import styled, { createGlobalStyle } from 'styled-components'
 import Header from './Header'
 import Footer from './Footer'
 import { breakpointDown } from '../utils/breakpoints'
+import { rgba, cssVar } from 'polished'
 
 const ResetStyle = createGlobalStyle`
   body,
@@ -217,7 +218,7 @@ const BaseStyle = createGlobalStyle`
     --base-selection-color: var(--color-white);
     --base-button-background-color: var(--color-black);
     --base-button-hover-background-color: var(--color-aqua);
-    --base-card-box-shadow: 0 1px 3px 1px rgba(66, 66, 66, 0.2), 0 2px 8px 4px rgba(66, 66, 66, 0.1);
+    --base-card-box-shadow: 0 1px 3px 1px ${rgba(cssVar('--color-gray-800'), 0.2)}, 0 2px 8px 4px ${rgba(cssVar('--color-gray-800'), 0.1)};
     --primary-color: var(--color-orange);
     --primary-border-color: var(--color-orange);
     --error-color: var(--color-red);
