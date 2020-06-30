cee22d6b628843d72845a825c638ee5ac41ff4bc
Toshifumi Imanishi <13635766+toshifumiimanishi@users.noreply.github.com>
2020-06-30 18:56:14 +0900

    Prettier 経由の自動コード整形

--- src/components/atoms/Icon.tsx
+++ src/components/atoms/Icon.tsx
@@ -3,7 +3,10 @@ import styled from 'styled-components'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import { faMoon } from '@fortawesome/free-solid-svg-icons'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon, FontAwesomeIconProps } from '@fortawesome/react-fontawesome'
+import {
+  FontAwesomeIcon,
+  FontAwesomeIconProps,
+} from '@fortawesome/react-fontawesome'
 
 type DOMProps = FontAwesomeIconProps
 
@@ -22,10 +25,7 @@ type Props = {
   title?: string
 }
 
-const IconDOM: React.FC<DOMProps> = ({
-  icon,
-  ...props
-}) => (
+const IconDOM: React.FC<DOMProps> = ({ icon, ...props }) => (
   <FontAwesomeIcon icon={icon} {...props} />
 )
 
@@ -42,19 +42,12 @@ const ContainerIcon: React.FC<ContainerProps> = ({
   }
   const icon = iconMap[name as keyof typeof iconMap]
   config.autoAddCss = false // Font Awesome のランタイムでの CSS 追加を無効にする
-  return presenter({ icon, ...props})
+  return presenter({ icon, ...props })
 }
 
-const Icon: React.FC<Props> = ({
-  icon,
-  ...props
-}) => (
+const Icon: React.FC<Props> = ({ icon, ...props }) => (
   <ContainerIcon
-    presenter={( presenterProps) => (
-      <PresentationalIcon
-        {...presenterProps}
-      />
-    )}
+    presenter={(presenterProps) => <PresentationalIcon {...presenterProps} />}
     icon={icon}
     {...props}
   />
